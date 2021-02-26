5d74dc5d9ac27f3c88a84249f54886341a72106a
Justin Lee <justlee006@gmail.com>
2021-02-25 19:08:56 -0800

    Add screenshots and fix main.test.js

--- Part1-Jest/__tests__/main.test.js
+++ Part1-Jest/__tests__/main.test.js
@@ -2,15 +2,15 @@ const formatVolumeIconPath = require('../assets/scripts/main');
 
 describe('tests for formatVolumeIconPath', () => {
     test('if value > 66', () => {
-        expect(formatVolumeIconPath(80).toMatch('./assets/media/icons/volume-level-3.svg'));
+        expect(formatVolumeIconPath(80)).toMatch('./assets/media/icons/volume-level-3.svg');
     });
     test('if 33 < val <= 66', () => {
-        expect(formatVolumeIconPath(40).toMatch('./assets/media/icons/volume-level-2.svg'));
+        expect(formatVolumeIconPath(40)).toMatch('./assets/media/icons/volume-level-2.svg');
     });
     test('if 0 < val <= 33', () => {
-        expect(formatVolumeIconPath(20).toMatch('./assets/media/icons/volume-level-1.svg'));
+        expect(formatVolumeIconPath(20)).toMatch('./assets/media/icons/volume-level-1.svg');
     });
     test('if value == 0', () => {
-        expect(formatVolumeIconPath(0).toMatch('./assets/media/icons/volume-level-0.svg'));
+        expect(formatVolumeIconPath(0)).toMatch('./assets/media/icons/volume-level-0.svg');
     });
 });
